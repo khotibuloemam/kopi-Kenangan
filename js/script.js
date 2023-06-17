06236803960f1d7a5f81bea156cf08bb92a16b65
Khotibul Umam <106510982+khotibuloemam@users.noreply.github.com>
2023-06-17 12:15:27 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toggle class menu
+
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
